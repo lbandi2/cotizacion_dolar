1e60a02ab5399100879cf540282b361ea8ebd3c6
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-24 16:10:12 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 mkdir -p logs
 cd /home/sergio/scripts/cotizacion_dolar/
+find logs -type f -mtime +30 -delete
 . ./env/bin/activate
 /home/sergio/scripts/cotizacion_dolar/env/bin/python3 /home/sergio/scripts/cotizacion_dolar/main.py >> logs/dolar-"`date +"%Y-%m-%d_%H.%M.%S"`".log 2>&1
 deactivate
